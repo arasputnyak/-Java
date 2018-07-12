14f0d9f4448a647336f527e85660a3da291af563
anastasia <rasputnyak_n@inbox.ru>
2018-07-12 15:58:44 +0300

    deleted

--- SimpleProjForSpring/src/main/java/lab/anastasya/account/AccountService.java
+++
@@ -1,9 +0,0 @@
-package lab.anastasya.account;
-
-import lab.anastasya.database.DBException;
-
-public interface AccountService {
-    MyUser getUserByName(String name) throws DBException;
-    // void addNewUser(String name, int age, String homeTown) throws DBException;
-    void addNewUser2(MyUser myUser) throws DBException;
-}
--- SimpleProjForSpring/src/main/java/lab/anastasya/database/DBException.java
+++
@@ -1,7 +0,0 @@
-package lab.anastasya.database;
-
-public class DBException extends Exception {
-    public DBException(Throwable throwable) {
-        super(throwable);
-    }
-}
